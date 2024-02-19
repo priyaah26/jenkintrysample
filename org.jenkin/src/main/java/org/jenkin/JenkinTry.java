c298961db1ac8e8c13c38629164406966bd29ae8
Admin <Admin@192.168.0.103>
2024-02-19 20:22:54 +0530

    initial commit

---
+++ org.jenkin/src/main/java/org/jenkin/JenkinTry.java
@@ -0,0 +1,22 @@
+package org.jenkin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JenkinTry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+		System.out.println("opened the browser");
+
+
+	}
+
+}
